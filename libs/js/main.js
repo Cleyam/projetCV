c91cdf413d5244928d13f9059b3d2c569dedd172
Cleyam <cleyam@gmail.com>
2019-12-22 17:35:23 +0100

    2nd commit

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,33 +1,35 @@
 $(function () {
+    // Append formation.json into HTML
     $.ajax({
-               url: "ajax/formation.json",
-               method: "GET"
-           }).done(function (data) {
+        url: "ajax/formation.json",
+        method: "GET"
+    }).done(function (data) {
         for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#formation").append("<div id=\'formation" + loop + "\' class=\'formation row\'>\n" +
-                                       "            <div id=\'periode" + loop + "\' class=\'col-4\'>" + data[loop].periode + "</div>\n" +
-                                       "            <div class=\'col-8\'>\n" +
-                                       "                <div  id=\'name" + loop + "\'>" + data[loop].name + "</div>\n" +
-                                       "                <div id=\'place" + loop + "\'>" + data[loop].place + "</div>\n" +
-                                       "            </div>\n" +
-                                       "        </div>"
+            $("#formation").append("<hr><div id=\'formation" + loop + "\' class=\'formation row\'>\n" +
+                "            <div id=\'periode" + loop + "\' class=\'col-12\'>" + data[loop].periode + "</div>\n" +
+                "            <div class=\'col-12\'>\n" +
+                "                <p  id=\'name" + loop + "\'class=\'font-weight-bold\'>" + data[loop].name + "</p>\n" +
+                "                <div id=\'place" + loop + "\'>" + data[loop].place + "</div>\n" +
+                "            </div>\n" +
+                "        </div>"
             )
 
         }
     });
+    // Append experience.json into HTML
     $.ajax({
-               url: "ajax/experience.json",
-               method: "GET"
-           }).done(function (data) {
+        url: "ajax/experience.json",
+        method: "GET"
+    }).done(function (data) {
         for (let loop = 0; loop <= data.length - 1; loop++) {
-            let ajout = "<div id=\'experience" + loop + "\' class=\'formation row\'>\n";
-            ajout += "<div id=\'periode" + loop + "\' class=\'col-4\'>" + data[loop].periode + "</div>\n";
-            ajout += "<div class=\'col-8\'>\n";
-            ajout += "<div  id=\'name" + loop + "\'>" + data[loop].name + "</div>\n";
-            ajout += "<div id=\'place" + loop + "\'>" + data[loop].place + "</div>\n";
-            ajout += "<div id=\'detail" + loop + "\'><ul class=\'list-unstyled\'>";
-             for (let loopLI = 0; loopLI<= data[loop].detail.length; loopLI++){
-                 ajout +=    "<li>"+data[loop].detail[loopLI]+"</li>"
+            let ajout = "<hr><div id=\'experience" + loop + "\' class=\'formation row\'>\n";
+            ajout += "<div id=\'periode" + loop + "\' class=\'col-12\'>" + data[loop].periode + "</div>\n";
+            ajout += "<div class=\'col-12\'>\n";
+            ajout += "<p  id=\'name" + loop + "\' class=\'font-weight-bold\'>" + data[loop].name + "</p>\n";
+            ajout += "<div id=\'place" + loop + "\' class=\'font-weight-bold\'>" + data[loop].place + "</div>\n";
+            ajout += "<div id=\'detail" + loop + "\'><ul class=\'text-left\'>";
+            for (let loopLI = 0; loopLI <= data[loop].detail.length - 1; loopLI++) {
+                ajout += "<li class=\'experienceDetail\'>" + data[loop].detail[loopLI] + "</li>"
             };
             ajout += "</ul></div>\n";
             ajout += "</div>\n";
@@ -36,79 +38,153 @@ $(function () {
 
         }
     });
+    // Append languages.json into HTML
     $.ajax({
-               url: "ajax/skills/languages.json",
-               method: "GET"
-           }).done(function (data) {
+        url: "ajax/skills/languages.json",
+        method: "GET"
+    }).done(function (data) {
         for (let loop = 0; loop <= data.length - 1; loop++) {
-        $("#languages").append("<ul id=\'language"+loop+"\' class=\'whiteText col-12\'>\n" +
-                               "                <li class=\'row justify-content-center col-12\'><i class=\'"+data[loop].iconClass+" cvIcons text-right col-4 px-0\'></i>\n" +
-                               "                    <p class=\'font-weight-bold my-auto col-3\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                               "            </ul>")
+            $("#languages").append("<ul id=\'language" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append frameworks.json into HTML
+    $.ajax({
+        url: "ajax/skills/frameworks.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#frameworks").append("<ul id=\'framework" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append skills.json into HTML
+    $.ajax({
+        url: "ajax/skills/technologies.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#technologies").append("<ul id=\'techno" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append devTools.json into HTML
+    $.ajax({
+        url: "ajax/skills/devTools.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#devTools").append("<ul id=\'devTool" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append graphicTools.json into HTML
+    $.ajax({
+        url: "ajax/skills/graphicTools.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#graphicTools").append("<ul id=\'graphicTool" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append desktopTools.json into HTML
+    $.ajax({
+        url: "ajax/skills/desktopTools.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#desktopTools").append("<ul id=\'desktopTool" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append spokenLanguages.json into HTML
+    $.ajax({
+        url: "ajax/skills/spokenLanguages.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#spokenLanguages").append("<ul id=\'spokenLanguage" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <p class=\'font-weight-bold text-dark my-auto col-5 list-unstyled\'>" + data[loop].name + "</p><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].info + "</p></li>\n" +
+                "            </ul>")
+
+        }
+    });
+    // Append cms.json into HTML
+    $.ajax({
+        url: "ajax/skills/cms.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#cms").append("<ul id=\'cms" + loop + "\' class=\'whiteText col-12 list-unstyled\'>\n" +
+                "                <li class=\'row justify-content-center\'>\n" +
+                "                    <i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i><p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p></li>\n" +
+                "            </ul>")
 
         }
     });
 
+    // Append profile.json into HTML
     $.ajax({
-               url: "ajax/skills/frameworks.json",
-               method: "GET"
-           }).done(function (data) {
+        url: "ajax/profile.json",
+        method: "GET"
+    }).done(function (data) {
         for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#frameworks").append("<ul id=\'framework"+loop+"\' class=\'whiteText col-12\'>\n" +
-                                       "                <li class=\'row justify-content-center col-12\'>\n" +
-                                       "                    <p class=\'font-weight-bold my-auto col-3 offset-4\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                                       "            </ul>")
-
-        }
-    });
-    $.ajax({
-               url: "ajax/skills/technologies.json",
-               method: "GET"
-           }).done(function (data) {
-        for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#technologies").append("<ul id=\'techno"+loop+"\' class=\'whiteText col-12\'>\n" +
-                                        "                <li class=\'row justify-content-center col-12\'>\n" +
-                                        "                    <p class=\'font-weight-bold my-auto col-3 offset-4\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                                        "            </ul>")
-
-        }
-    });
-    $.ajax({
-               url: "ajax/skills/devTools.json",
-               method: "GET"
-           }).done(function (data) {
-        for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#devTools").append("<ul id=\'devTool"+loop+"\' class=\'whiteText col-12\'>\n" +
-                                        "                <li class=\'row justify-content-center col-12\'>\n" +
-                                        "                    <p class=\'font-weight-bold my-auto col-3 offset-4\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                                        "            </ul>")
-
-        }
-    });
-    $.ajax({
-               url: "ajax/skills/graphicTools.json",
-               method: "GET"
-           }).done(function (data) {
-        for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#graphicTools").append("<ul id=\'graphicTool"+loop+"\' class=\'whiteText col-12\'>\n" +
-                                        "                <li class=\'row justify-content-center col-12\'>\n" +
-                                        "                    <p class=\'font-weight-bold my-auto col-3 offset-4\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                                        "            </ul>")
-
-        }
-    });
-    $.ajax({
-               url: "ajax/skills/desktopTools.json",
-               method: "GET"
-           }).done(function (data) {
-        for (let loop = 0; loop <= data.length - 1; loop++) {
-            $("#desktopTools").append("<ul id=\'desktopTool"+loop+"\' class=\'whiteText col-12\'>\n" +
-                                        "                <li class=\'row justify-content-center col-12\'>\n" +
-                                        "                    <p class=\'font-weight-bold my-auto col-3 offset-4\'>"+data[loop].name+"</p><p class=\'col-3\'>"+data[loop].level+"</p></li>\n" +
-                                        "            </ul>")
+            $("#profile").append("<div>\n" +
+                "   <h3>" + data[loop].name + "</h3>\n" +
+                "   <div>\n" +
+                "        <p>" + data[loop].info + "</p>\n" +
+                "    </div>\n" +
+                "</div>")
 
         }
     });
 
+    // Append Header into HTML
+    $("#header").append("<h1 class=\'col-12\'>Curriculum Vitae<br><span class=\'font-weight-bold\'>Thomas HERMANT</span></h1>\n" +
+        "<img id=\'profilePicture\' src=\'libs/img/profilePicture.jpg\' alt=\'Photo Thomas Hermant\'\n" +
+        "    title=\'Photo Thomas Hermant\'>\n" +
+        " <p id=\'intro\' class=\'col-12 font-italic\'>Développeur Web & Web Mobile junior, diplômé de lettres, avec l\'expérience d'internet et de la vie en entreprise.</p>");
+
+    // Menu Accordeon JQuery UI
+    $("#accordion").accordion({
+        heightStyle: "content"
+    });
+
+    // Append formation.json into HTML
+    $.ajax({
+        url: "ajax/hobbies.json",
+        method: "GET"
+    }).done(function (data) {
+        for (let loop = 0; loop <= data.length - 1; loop++) {
+            $("#hobbies").append("<hr><div id=\'hobby" + loop + "\' class=\'hobbies row\'>\n" +
+                "            <div id=\'name" + loop + "\' class=\'col-12 font-weight-bold\'>" + data[loop].name + "</div>\n" +
+                "            <div class=\'col-12\'>\n" +
+                "                <p  id=\'detail" + loop + "\'>" + data[loop].detail + "</p>\n" +
+                "            </div>\n" +
+                "        </div>"
+            )
+
+        }
+    });
 
 });
