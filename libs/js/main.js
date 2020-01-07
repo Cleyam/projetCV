159c8310c5a54667104f2ca01fb1300e4221071f
Cleyam <cleyam@gmail.com>
2020-01-07 15:43:37 +0100

    commit5

--- libs/js/main.js
+++ libs/js/main.js
@@ -4,16 +4,15 @@ function comp(a, b, c) {
                url: a,
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-
-            $(b).append("<ul id=\'" + c + "" + loop + "\' class=\'whiteText col-12 list-unstyled\'><li class=\'row justify-content-center\'>" + (data[loop].iconClass != undefined ? "<i class=\'" + data[loop].iconClass + " cvIcons text-right col-5 px-0\'></i>" : "") + "<p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + data[loop].name + "</p>" + (data[loop].info != undefined ? "<p class=\' text-dark my-auto col-5 list-unstyled\'>" + data[loop].info + "</p>" : "") + "</li></ul>")
-        }
+        $.each(data, function(key, val){
+            $(b).append("<ul id=\'" + c + "" + key + "\' class=\'whiteText col-12 list-unstyled\'><li class=\'row justify-content-center\'>" + (val.iconClass != undefined ? "<i class=\'" + val.iconClass + " cvIcons text-right col-5 px-0\'></i>" : "") + "<p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + val.name + "</p>" + (val.info != undefined ? "<p class=\' text-dark my-auto col-5 list-unstyled\'>" + val.info + "</p>" : "") + "</li></ul>")
+        })
     });
 }
 
 $(function () {
     // Append structure into HTML
-    $("body").prepend("<div id=\"structure\" class=\"container-fluid px-0\"><header id=\"header\" class=\"row text-center bgDarkBlue text-light justify-content-center shadow-sm\"></header><div class=\"d-md-flex justify-content-around\"><div id=\"leftBarMd\" class=\"col-md-4\"><section id=\"profile\" class=\"justify-content-center col-12 bg-light px-0 text-center shadow-sm\"></section><section id=\"accordion\" class=\"col-12\"></section></div><div class=\"col-md-8\"><section id=\"formation\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"experience\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"hobbies\" class=\"col-12 text-center bg-light shadow-sm\"></section></div></div></div>");
+    $("body").prepend("<div id=\"structure\" class=\"container-fluid px-0\"><header id=\"header\" class=\"row text-center bgDarkBlue text-light justify-content-center shadow-sm\"></header><div class=\"d-md-flex justify-content-around\"><div id=\"leftBarMd\" class=\"col-md-4\"><section id=\"profile\" class=\"justify-content-center col-12 bg-light px-0 text-center shadow-sm\"></section><section id=\"accordion\" class=\"col-12\"></section></div><div class=\"col-md-8\"><section id=\"formation\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"experience\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"hobbies\" class=\"col-12 text-center bg-light shadow-sm\"></section></div></div><div id='contact' class='justify-content-center bg-light'></div></div>");
 
     // Append Header into HTML
     $("#header").append("<h1 class=\'col-12\'>Curriculum Vitae<br><span class=\'font-weight-bold\'>Thomas HERMANT</span></h1><img id=\'profilePicture\' src=\'libs/img/profilePicture.jpg\' alt=\'Photo Thomas Hermant\' title=\'Photo Thomas Hermant\'><p id=\'intro\' class=\'col-12 font-italic\'>Développeur Web & Web Mobile junior, diplômé de lettres, avec l\'expérience d'internet et de la vie en entreprise.</p>");
@@ -23,9 +22,9 @@ $(function () {
                url: "ajax/profile.json",
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-            $("#profile").append("<div><h3>" + data[loop].name + "</h3><div><p>" + data[loop].info + "</p></div></div>")
-        }
+        $.each(data, function(key, val){
+            $("#profile").append("<div><h3>" + val.name + "</h3><div><p>" + val.info + "</p></div></div>")
+        })
     });
 
     // Append accordion into HTML
@@ -52,9 +51,9 @@ $(function () {
                url: "ajax/formation.json",
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-            $("#formation").append("<hr><div id=\'formation" + loop + "\' class=\'formation row\'><div id=\'periode" + loop + "\' class=\'col-12\'>" + data[loop].periode + "</div><div class=\'col-12\'><p  id=\'name" + loop + "\'class=\'font-weight-bold\'>" + data[loop].name + "</p><div id=\'place" + loop + "\'>" + data[loop].place + "</div></div></div>")
-        }
+        $.each(data, function(key, val){
+            $("#formation").append("<hr><div id=\'formation" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\'class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\'>" + val.place + "</div></div></div>")
+        })
     });
 
     // Append experience.json into HTML
@@ -63,14 +62,14 @@ $(function () {
                url: "ajax/experience.json",
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-            let ajout = "<hr><div id=\'experience" + loop + "\' class=\'formation row\'><div id=\'periode" + loop + "\' class=\'col-12\'>" + data[loop].periode + "</div><div class=\'col-12\'><p  id=\'name" + loop + "\' class=\'font-weight-bold\'>" + data[loop].name + "</p><div id=\'place" + loop + "\' class=\'font-weight-bold\'>" + data[loop].place + "</div><div id=\'detail" + loop + "\'><ul class=\'text-left\'>";
-            for (let loopLI = 0; loopLI < data[loop].detail.length; loopLI++) {
-                ajout += "<li class=\'experienceDetail\'>" + data[loop].detail[loopLI] + "</li>"
+        $.each(data, function(key, val){
+            let ajout = "<hr><div id=\'experience" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\' class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\' class=\'font-weight-bold\'>" + val.place + "</div><div id=\'detail" + key + "\'><ul class=\'text-left\'>";
+            for (let loopLI = 0; loopLI < val.detail.length; loopLI++) {
+                ajout += "<li class=\'experienceDetail\'>" + val.detail[loopLI] + "</li>"
             }
             ajout += "</ul></div></div></div>";
             $("#experience").append(ajout)
-        }
+        })
     });
 
     // Append hobbies.json into HTML
@@ -79,9 +78,30 @@ $(function () {
                url: "ajax/hobbies.json",
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-            $("#hobbies").append("<hr><div id=\'hobby" + loop + "\' class=\'hobbies row\'><div id=\'name" + loop + "\' class=\'col-12 font-weight-bold\'>" + data[loop].name + "</div><div class=\'col-12\'><p  id=\'detail" + loop + "\'>" + data[loop].detail + "</p></div></div>")
-        }
+        $.each(data, function(key, val){
+            $("#hobbies").append("<hr><div id=\'hobby" + key + "\' class=\'hobbies row\'><div id=\'name" + key + "\' class=\'col-12 font-weight-bold\'>" + val.name + "</div><div class=\'col-12\'><p  id=\'detail" + key + "\'>" + val.detail + "</p></div></div>")
+        })
     });
+
+    // Insert contact form into HTML on scroll to page bottom
+    $.ajax({
+               url: "ajax/contact.html",
+               method: "GET"
+           }).done(function (data) {
+        let SetValues=function(){
+            $('table tr:first-child td:nth-child(2)').html($(window).scrollTop());
+            $('table tr:first-child td:nth-child(4)').html($(document).height());
+            $('table tr:first-child td:nth-child(6)').html(window.innerHeight);//$(window).height()
+        };
+        $(window).on("scroll", function(){
+            //Will check if the user has reached bottom of a PAGE
+            SetValues();
+            //Check for user has reached bottom of Page
+            if($(window).scrollTop()==($(document).height()-window.innerHeight)){
+                $("#contact").html(data)
+            }
+        });
+        })
+    
 });
 
