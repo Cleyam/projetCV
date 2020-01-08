d68d73fab983076c767e0d50309bd6fa0a0fab8e
Cleyam <cleyam@gmail.com>
2020-01-08 12:56:12 +0100

    commit6

--- libs/js/main.js
+++ libs/js/main.js
@@ -4,7 +4,7 @@ function comp(a, b, c) {
                url: a,
                method: "GET"
            }).done(function (data) {
-        $.each(data, function(key, val){
+        $.each(data, function (key, val) {
             $(b).append("<ul id=\'" + c + "" + key + "\' class=\'whiteText col-12 list-unstyled\'><li class=\'row justify-content-center\'>" + (val.iconClass != undefined ? "<i class=\'" + val.iconClass + " cvIcons text-right col-5 px-0\'></i>" : "") + "<p class=\'font-weight-bold text-dark my-auto col-7 list-unstyled\'>" + val.name + "</p>" + (val.info != undefined ? "<p class=\' text-dark my-auto col-5 list-unstyled\'>" + val.info + "</p>" : "") + "</li></ul>")
         })
     });
@@ -22,7 +22,7 @@ $(function () {
                url: "ajax/profile.json",
                method: "GET"
            }).done(function (data) {
-        $.each(data, function(key, val){
+        $.each(data, function (key, val) {
             $("#profile").append("<div><h3>" + val.name + "</h3><div><p>" + val.info + "</p></div></div>")
         })
     });
@@ -40,18 +40,18 @@ $(function () {
                url: "ajax/skills/skills.json",
                method: "GET"
            }).done(function (data) {
-        for (let loop = 0; loop < data.length; loop++) {
-            comp(data[loop].json, data[loop].anchor, data[loop].ul);
-        }
+        $.each(data, function (key, val) {
+            comp(val.json, val.anchor, val.ul);
+        });
     });
 
     // Append formation.json into HTML
-    $("#formation").append("<h3 class=\"font-weight-bold\">Formations</h3>");
+    $("#formation").append($("<h3>", {class: "font-weight-bold", text: "Formations"}));
     $.ajax({
                url: "ajax/formation.json",
                method: "GET"
            }).done(function (data) {
-        $.each(data, function(key, val){
+        $.each(data, function (key, val) {
             $("#formation").append("<hr><div id=\'formation" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\'class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\'>" + val.place + "</div></div></div>")
         })
     });
@@ -62,7 +62,7 @@ $(function () {
                url: "ajax/experience.json",
                method: "GET"
            }).done(function (data) {
-        $.each(data, function(key, val){
+        $.each(data, function (key, val) {
             let ajout = "<hr><div id=\'experience" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\' class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\' class=\'font-weight-bold\'>" + val.place + "</div><div id=\'detail" + key + "\'><ul class=\'text-left\'>";
             for (let loopLI = 0; loopLI < val.detail.length; loopLI++) {
                 ajout += "<li class=\'experienceDetail\'>" + val.detail[loopLI] + "</li>"
@@ -78,30 +78,51 @@ $(function () {
                url: "ajax/hobbies.json",
                method: "GET"
            }).done(function (data) {
-        $.each(data, function(key, val){
+        $.each(data, function (key, val) {
             $("#hobbies").append("<hr><div id=\'hobby" + key + "\' class=\'hobbies row\'><div id=\'name" + key + "\' class=\'col-12 font-weight-bold\'>" + val.name + "</div><div class=\'col-12\'><p  id=\'detail" + key + "\'>" + val.detail + "</p></div></div>")
         })
     });
 
     // Insert contact form into HTML on scroll to page bottom
+    let contactLoaded = false;
     $.ajax({
                url: "ajax/contact.html",
                method: "GET"
            }).done(function (data) {
-        let SetValues=function(){
-            $('table tr:first-child td:nth-child(2)').html($(window).scrollTop());
-            $('table tr:first-child td:nth-child(4)').html($(document).height());
-            $('table tr:first-child td:nth-child(6)').html(window.innerHeight);//$(window).height()
-        };
-        $(window).on("scroll", function(){
-            //Will check if the user has reached bottom of a PAGE
-            SetValues();
-            //Check for user has reached bottom of Page
-            if($(window).scrollTop()==($(document).height()-window.innerHeight)){
-                $("#contact").html(data)
+        $(window).on("scroll", function () {
+            if (!contactLoaded) {
+                let checkHeight = function () {
+                    $('table tr:first-child td:nth-child(2)').html($(window).scrollTop());
+                    $('table tr:first-child td:nth-child(4)').html($(document).height());
+                    $('table tr:first-child td:nth-child(6)').html(window.innerHeight);//$(window).height()
+                };
+                checkHeight();
+                if ($(window).scrollTop() == ($(document).height() - window.innerHeight)) {
+                    $("#contact").html(data);
+                    contactLoaded = true;
+                    // Generate sub-select on selection
+                    $.ajax({
+                               url: "ajax/contactSelect.json",
+                               method: "GET"
+                           }).done(function (data) {
+                        $("#objet").on("change", function () {
+                            $("#subSelect").html("");
+                            for (let loop = 0; loop < data.length; loop++) {
+                                if ($("#objet").val() == data[loop].id) {
+                                    $("#subSelect").append($("<select>", {id: "subObject", class: "form-control"}));
+                                    $.each(data[loop].options, function (key, val) {
+                                        $("#subObject").append($("<option>", {value: key, text: val}))
+                                    });
+
+                                }
+                            }
+                        })
+                    })
+                }
             }
         });
-        })
-    
+    });
+
+
 });
 
