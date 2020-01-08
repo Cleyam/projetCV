4ce83a91c490d3b8ca3342aa149f48c12ccab2b9
Cleyam <cleyam@gmail.com>
2020-01-08 16:09:39 +0100

    commit7

--- libs/js/main.js
+++ libs/js/main.js
@@ -15,7 +15,7 @@ $(function () {
     $("body").prepend("<div id=\"structure\" class=\"container-fluid px-0\"><header id=\"header\" class=\"row text-center bgDarkBlue text-light justify-content-center shadow-sm\"></header><div class=\"d-md-flex justify-content-around\"><div id=\"leftBarMd\" class=\"col-md-4\"><section id=\"profile\" class=\"justify-content-center col-12 bg-light px-0 text-center shadow-sm\"></section><section id=\"accordion\" class=\"col-12\"></section></div><div class=\"col-md-8\"><section id=\"formation\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"experience\" class=\"col-12 text-center bg-light shadow-sm\"></section><section id=\"hobbies\" class=\"col-12 text-center bg-light shadow-sm\"></section></div></div><div id='contact' class='justify-content-center bg-light'></div></div>");
 
     // Append Header into HTML
-    $("#header").append("<h1 class=\'col-12\'>Curriculum Vitae<br><span class=\'font-weight-bold\'>Thomas HERMANT</span></h1><img id=\'profilePicture\' src=\'libs/img/profilePicture.jpg\' alt=\'Photo Thomas Hermant\' title=\'Photo Thomas Hermant\'><p id=\'intro\' class=\'col-12 font-italic\'>Développeur Web & Web Mobile junior, diplômé de lettres, avec l\'expérience d'internet et de la vie en entreprise.</p>");
+    $("#header").append("<h1 class=\'col-12\'>Curriculum Vitae<br><span class=\'font-weight-bold\'>Thomas HERMANT</span></h1><img id=\'profilePicture\' src=\'libs/img/profilePicture.png\' alt=\'Photo Thomas Hermant\' title=\'Photo Thomas Hermant\'><p id=\'intro\' class=\'col-12 font-italic\'>Développeur Web & Web Mobile junior, diplômé de lettres, avec l\'expérience d'internet et de la vie en entreprise.</p>");
 
     // Append profile.json into HTML
     $.ajax({
@@ -28,12 +28,13 @@ $(function () {
     });
 
     // Append accordion into HTML
-    $("#accordion").append(" <h3>Langages</h3><div id=\"languages\" class=\"col-12 px-0\"></div><h3>Frameworks</h3><div id=\"frameworks\" class=\"col-12 px-0\"></div><h3>Technologies</h3><div id=\"technologies\" class=\"col-12 px-0\"></div><h3>CMS</h3><div id=\"cms\" class=\"col-12 px-0\"></div><h3>Outils de développement</h3><div id=\"devTools\" class=\"col-12 px-0\"></div><h3>Outils de graphisme</h3><div id=\"graphicTools\" class=\"col-12 px-0\"></div><h3>Outils bureautiques</h3><div id=\"desktopTools\" class=\"col-12 px-0\"></div><h3>Langues</h3><div id=\"spokenLanguages\" class=\"col-12 px-0\"></div>");
+    let accordion = $("#accordion");
+    accordion.append(" <h3>Langages</h3><div id=\"languages\" class=\"col-12 px-0\"></div><h3>Frameworks</h3><div id=\"frameworks\" class=\"col-12 px-0\"></div><h3>Technologies</h3><div id=\"technologies\" class=\"col-12 px-0\"></div><h3>CMS</h3><div id=\"cms\" class=\"col-12 px-0\"></div><h3>Outils de développement</h3><div id=\"devTools\" class=\"col-12 px-0\"></div><h3>Outils de graphisme</h3><div id=\"graphicTools\" class=\"col-12 px-0\"></div><h3>Outils bureautiques</h3><div id=\"desktopTools\" class=\"col-12 px-0\"></div><h3>Langues</h3><div id=\"spokenLanguages\" class=\"col-12 px-0\"></div>");
 
     // Menu Accordeon JQuery UI
-    $("#accordion").accordion({
-                                  heightStyle: "content"
-                              });
+    accordion.accordion({
+                            heightStyle: "content"
+                        });
 
     // Append skill tables into HTML
     $.ajax({
@@ -52,7 +53,7 @@ $(function () {
                method: "GET"
            }).done(function (data) {
         $.each(data, function (key, val) {
-            $("#formation").append("<hr><div id=\'formation" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\'class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\'>" + val.place + "</div></div></div>")
+            $("#formation").append("<hr><div id=\'formation" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\' class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\'>" + val.place + "</div></div></div>")
         })
     });
 
@@ -63,13 +64,27 @@ $(function () {
                method: "GET"
            }).done(function (data) {
         $.each(data, function (key, val) {
-            let ajout = "<hr><div id=\'experience" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\' class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\' class=\'font-weight-bold\'>" + val.place + "</div><div id=\'detail" + key + "\'><ul class=\'text-left\'>";
-            for (let loopLI = 0; loopLI < val.detail.length; loopLI++) {
-                ajout += "<li class=\'experienceDetail\'>" + val.detail[loopLI] + "</li>"
-            }
-            ajout += "</ul></div></div></div>";
-            $("#experience").append(ajout)
+            let ajout = "<hr><div id=\'experience" + key + "\' class=\'formation row\'><div id=\'periode" + key + "\' class=\'col-12\'>" + val.periode + "</div><div class=\'col-12\'><p  id=\'name" + key + "\' class=\'font-weight-bold\'>" + val.name + "</p><div id=\'place" + key + "\' class=\'font-weight-bold\'>" + val.place + "</div><div id=\'detail" + key + "\'><button id=\'buttonDetail" + key + "\' class=\'btn btn-primary bgDarkBlue btn-lg btnMission\'>Missions</button></div></div></div>";
+
+            $("#experience").append(ajout);
+            $("#buttonDetail" + key).on("click", function () {
+                let popin = $("<ul>", {id: "detailList"});
+                for (let loop = 0; loop < val.detail.length; loop++) {
+                    popin.append("<li class=\'experienceDetail\'>" + val.detail[loop] + "</li>")
+                }
+                popin.dialog({
+                                 classes: {
+                                     "ui-dialog-titlebar": "bgDarkBlue",
+                                     "ui-dialog": "modalWidth"
+                                 },
+                                 position: {
+                                     my: "center", at: "center"
+                                 },
+                                 resizable: false
+                             });
+            });
         })
+
     });
 
     // Append hobbies.json into HTML
@@ -91,12 +106,6 @@ $(function () {
            }).done(function (data) {
         $(window).on("scroll", function () {
             if (!contactLoaded) {
-                let checkHeight = function () {
-                    $('table tr:first-child td:nth-child(2)').html($(window).scrollTop());
-                    $('table tr:first-child td:nth-child(4)').html($(document).height());
-                    $('table tr:first-child td:nth-child(6)').html(window.innerHeight);//$(window).height()
-                };
-                checkHeight();
                 if ($(window).scrollTop() == ($(document).height() - window.innerHeight)) {
                     $("#contact").html(data);
                     contactLoaded = true;
@@ -113,7 +122,6 @@ $(function () {
                                     $.each(data[loop].options, function (key, val) {
                                         $("#subObject").append($("<option>", {value: key, text: val}))
                                     });
-
                                 }
                             }
                         })
@@ -125,4 +133,3 @@ $(function () {
 
 
 });
-
